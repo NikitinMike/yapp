8af32e239502963e26235848acba82d0f1d59227
Mike <mike@digt.ru>
2018-11-27 14:21:21 +0300

    data outside

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react'
 import Tab from './Table';
 import './App.css'
 
-function WelcomeF(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
 class WelcomeC extends React.Component {
-  render() {
-    return <h1> Hello, {this.props.name} </h1>;
-  }
+  render() {return <h1> Hello, {this.props.name} !</h1>;}
 }
+
+const data=[
+  ['Вася',100,101+55],
+  ['Петя',100,102+64],
+  ['Жора',100,103+73],
+  ['Коля',100,104+82],
+  ['Лёша',100,105+91]
+]
+
+function WelcomeF(props) {return <h1>Hello, {props.name}!</h1>;}
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <WelcomeC name="MikeN"/>
-        <Tab header= {['Мия','Вес','Рост']}
-            data={[['Вася',100,110],['Петя',100,120],['Жора',100,130],['Коля',100,140],['Лёша',100,150]]} 
-        />
+        <WelcomeF name="Kitty"/>
+        <Tab header= {['Мия','Вес','Рост']} data={data}/>
       </div>
     )
   }
