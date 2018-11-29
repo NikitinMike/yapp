7e725e819a9ec7a3243e378c8cba759541a57787
Mike <mike@digt.ru>
2018-11-29 11:21:23 +0300

    load json objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import AccountsTable from './AccountsTable';
 import LicensesTable from './LicensesTable';
-// import './App.css'
-import accountsJSON from './accounts.json';
-import licensesJSON from './licenses.json';
+import './App.css'
+// import accountsJSON from './accounts.json';
+// import licensesJSON from './licenses.json';
+// import accountsJSON from 'https://licensesvc.trusted.ru/license/jwt/accounts';
 
-const accounts = accountsJSON.data;
-const licenses = licensesJSON.data
+// var accounts = accountsJSON.data;
+// var licenses = licensesJSON.data;
+var accounts =[]
+var licenses =[]
 
 // function WelcomeF(props) {return <h1>Hello, {props.name}!</h1>;}
 
@@ -18,21 +21,27 @@ class App extends Component {
 
   componentDidMount() {
     // https://habr.com/post/252941/
+    // console.log("ACCOUNTS")
     fetch('https://licensesvc.trusted.ru/license/jwt/accounts', {mode: 'cors'})
       .then(function(response) {
-        console.log("ACCOUNTS")
-        console.log(response.headers.get('Content-Type'));  
-        console.log(response.headers.get('Date'));
-        console.log(response.status);  
-        console.log(response.statusText);  
-        console.log(response.type);  
-        console.log(response.url);  
-    });
+      // console.log(response.headers.get('Content-Type'));  
+      // console.log(response.headers.get('Date'));
+      console.log(response.url,response.status+"/"+response.statusText);  
+      // console.log(response.type);  
+      // console.log(response.text());
+      return response.text(); })
+      .then(function(message) { 
+        accounts = JSON.parse(message).data;
+        console.log('ACCOUNTS:',accounts)
+      })
+      .catch(function(error) { console.log('Request failed', error) });
 
-    console.log("LICENSES")
     fetch('https://licensesvc.trusted.ru/license/jwt/licenses', {mode: 'cors'})
-      .then(function(response) { return response.text(); })  
-      .then(function(text) { console.log('Request successful', text); })  
+      .then(function(response) { return response.text(); })
+      .then(function(text) { 
+        licenses = JSON.parse(text).data;
+        console.log('LICENSES:',licenses)
+      })
       .catch(function(error) { console.log('Request failed', error) });
 
     /*
