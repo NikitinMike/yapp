a6a4399f26a4db9806225cbd784e85417674259b
Mike <mike@digt.ru>
2018-11-29 16:50:07 +0300

    remove legacy code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import './App.css'
-// import * as React from "react";
+import * as React from "react";
 // import * as ReactDOM from "react-dom";
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import AccountsTable from './AccountsTable';
 import LicensesTable from './LicensesTable';
-import accountsJSON from './accounts.json';
-import licensesJSON from './licenses.json';
 // import accountsJSON from 'https://licensesvc.trusted.ru/license/jwt/accounts';
 
-var accounts = accountsJSON.data;
-var licenses = licensesJSON.data;
-// var accounts =[]
-// var licenses =[]
-// this.state.accounts=accounts
-// this.state.licenses=licenses
-// this.state = {accounts:[]};
-// this.state = {licenses:[]};
-
 // function WelcomeF(props) {return <h1>Hello, {props.name}!</h1>;}
-// interface AppState {}
-// interface AppProps {}
 class WelcomeC extends React.Component {
   render() {return <h1> Hello, {this.props.name} !</h1>;}
 }
@@ -27,67 +14,33 @@ class WelcomeC extends React.Component {
 class App extends React.Component {
 // class App extends React.Component<AppProps, AppState> {
 
-   accounts = accountsJSON.data;
-   licenses = licensesJSON.data;
-  
   constructor(props) {
     super(props)
     this.state = {accounts:[],licenses:[]};
   }
 
   componentDidMount() {
-    // https://habr.com/post/252941/
-    // console.log("ACCOUNTS")
-    this.setState({accounts: [] });
-    this.setState({licenses: [] });
     fetch('https://licensesvc.trusted.ru/license/jwt/accounts', {mode: 'cors'})
-      .then(function(response) {
-        // console.log(response.headers.get('Content-Type'));  
-        // console.log(response.headers.get('Date'));
-        console.log(response.url,response.status+"/"+response.statusText);  
-        // console.log(response.type);  
-        // console.log(response.text());
-        return response.text(); })
+      .then(response => { return response.text(); })
+        // console.log(response.url,response.status+"/"+response.statusText);
       .then(message => { 
         const accounts = JSON.parse(message).data;
-        console.log('ACCOUNTS:',accounts)
-        // accounts => this.setState({ accounts })
-        // this.state.accounts=accounts
-        // accounts => 
+        // console.log('ACCOUNTS:',accounts)
         this.setState({accounts});
       })
       .catch(function(error) { console.log('Request failed', error) });
-
     fetch('https://licensesvc.trusted.ru/license/jwt/licenses', {mode: 'cors'})
-      .then(function(response) { return response.text(); })
+      .then(response => { return response.text(); })
       .then(text => { 
-        licenses = JSON.parse(text).data;
-        console.log('LICENSES:',licenses)
-        // licenses => this.setState({licenses})
-        // this.state.licenses=licenses
+        const licenses = JSON.parse(text).data;
+        // console.log('LICENSES:',licenses)
         this.setState({licenses});
       })
       .catch(function(error) { console.log('Request failed', error) });
-    /*
-      .then(
-        function(response) {
-          console.log("FETCH!")
-          if (response.status !== 200) {  
-            console.log('Looks like there was a problem. Status Code: ' + response.status);  
-            return;  
-          }
-          // Examine the text in the response  
-          console.log(response.json)
-          response.json().then(function(data) { console.log(data); });  
-        }  
-      )
-      .catch(function(err) { console.log('Fetch Error :-S', err);});
-      */
     }
 
   render () {
-    // const {tst} = this.state;
-    console.log(this.state)
+    // console.log(this.state)
     return (
       <div className='App'>
         <WelcomeC name="Kitty"/>
@@ -98,5 +51,4 @@ class App extends React.Component {
   }
 }
 
-// ReactDOM.render(<App />, document.getElementById("container"));
 export default App
---
+++ src/didMount.jsx
@@ -0,0 +1,55 @@
+
+  componentDidMount() {
+    // fetch('https://api.mydomain.com')
+    fetch('https://licensesvc.trusted.ru/license/jwt/accounts', {mode: 'cors'})
+      .then(response => response.text())
+      .then(accounts => this.setState({accounts:  JSON.parse(response).data }));
+    fetch('https://licensesvc.trusted.ru/license/jwt/licenses', {mode: 'cors'})
+      .then(response => response.text())
+      .then(licenses => this.setState({licenses:  JSON.parse(response).data }));
+  }
+
+  componentDidMount() {
+    // https://habr.com/post/252941/
+    // console.log("ACCOUNTS")
+    fetch('https://licensesvc.trusted.ru/license/jwt/accounts', {mode: 'cors'})
+      .then(function(response) {
+      // console.log(response.headers.get('Content-Type'));  
+      // console.log(response.headers.get('Date'));
+      console.log(response.url,response.status+"/"+response.statusText);  
+      // console.log(response.type);  
+      // console.log(response.text());
+      return response.text(); })
+      .then(function(message) { 
+        accounts = JSON.parse(message).data;
+        console.log('ACCOUNTS:',accounts)
+        // accounts => this.setState({ accounts })
+        // this.state.accounts=accounts
+      })
+      .catch(function(error) { console.log('Request failed', error) });
+
+    fetch('https://licensesvc.trusted.ru/license/jwt/licenses', {mode: 'cors'})
+      .then(function(response) { return response.text(); })
+      .then(function(text) { 
+        licenses = JSON.parse(text).data;
+        console.log('LICENSES:',licenses)
+        // licenses => this.setState({licenses})
+        // this.state.licenses=licenses
+      })
+      .catch(function(error) { console.log('Request failed', error) });
+    /*
+      .then(
+        function(response) {
+          console.log("FETCH!")
+          if (response.status !== 200) {  
+            console.log('Looks like there was a problem. Status Code: ' + response.status);  
+            return;  
+          }
+          // Examine the text in the response  
+          console.log(response.json)
+          response.json().then(function(data) { console.log(data); });  
+        }  
+      )
+      .catch(function(err) { console.log('Fetch Error :-S', err);});
+      */
+    }
