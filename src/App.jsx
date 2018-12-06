6ad28da175fb15af755d693849593fdcc84bb112
Mike <mike@digt.ru>
2018-12-06 17:03:23 +0300

    big text render

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import './App.css'
 import React from 'react'
 import AccountsTable from './AccountsTable';
 import LicensesTable from './LicensesTable';
+
+// import {h, render} from 'ink';
+// import Gradient from 'ink-gradient';
+// import BigText from 'ink-big-text';
+
 const siteUrl='https://licensesvc.trusted.ru/license/account/'
-function Welcome(props) {return <h1 hidden>Hello, {props.name}!</h1>;}
+function Welcome(props) { return <h1 hidden>Hello, {props.name}!</h1>;}
+// function Welcome(props) { 
+//   return (
+//     <div>
+//       <Gradient name="rainbow">
+//         <BigText text="unicorns"/>
+//       </Gradient>    
+//     </div>        
+//   )
+// }
 
 class App extends React.Component {
   render () {
